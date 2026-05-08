0272850bb6304d6a78e2c22c1bef6f1aa2979f88
discover-hysa <discover@hysa.charity>
2026-05-07 23:27:52 -0700

    Delete hands_of_youth_photo_website directory

--- hands_of_youth_photo_website/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-const navButton = document.querySelector(".nav_toggle");
-const navLinks = document.querySelector(".nav_links");
-
-if (navButton && navLinks) {
-  navButton.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-  });
-}
